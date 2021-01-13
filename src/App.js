25f6a9d12623ababdf1944c7ec16d6aaa5ede6af
Rafaela Alves <rafaelaalves.rla@gmail.com>
2021-01-13 18:22:36 +0000

    header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import './App.css';
-//import Api from './api/Api';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
+
+
+
+//building my app body
 function App() {
-  return <div></div>
+  return <div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+  </div>;
+  
 }
+
 
 export default App;
 
 
-
-
-
-
-
-
-/* PRA TESTAR A API //////////////////////
-
-function App() {
-  return (
-    <div>
-      <Api />
-    </div>
-  );
-}
-*/
