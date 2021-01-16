9e1d3f7b09854a49e6acd60bd829f542eed1844f
Rafaela Alves <rafaelaalves.rla@gmail.com>
2021-01-16 13:40:31 +0000

    login page

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -4,6 +4,10 @@ import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
 import { UserContext } from '../../UserContext';
+import Error from '../Interface/Error';
+import styles from './LoginForm.module.css';
+import stylesBtn from '../Forms/Button.module.css';
+
 
 const LoginForm = () => {
   const username = useForm();
@@ -20,19 +24,28 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" {...username} />
-        <Input label="Senha" type="password" name="password" {...password} />
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input label="User name" type="text" name="username" {...username} />
+        <Input label="Password" type="password" name="password" {...password} />
         {loading ? (
           <Button disabled>Loading...</Button>
         ) : (
         <Button>Enter</Button>
         )}
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
-      <Link to="/login/create">Register</Link>
+      <Link className={styles.reset} to="/login/reset">
+        Have lost your password?
+      </Link>
+      <div className={styles.register}>
+        <h2 className={styles.subtitle}>Register here</h2>
+        <p>Still don't have an account? Register on our website</p>
+        <Link className={stylesBtn.button} to="/login/create">
+          Register
+        </Link>
+      </div>
     </section>
   );
 };
